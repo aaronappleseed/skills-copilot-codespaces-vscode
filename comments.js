fa41791b219c2e366a8a0210ea2666091a0b17cb
aaronappleseed <noseebnoraa@gmail.com>
2025-02-01 16:52:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,27 @@
 //create web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
+var http = require('http');
+var url = require('url');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//create server object
+http.createServer(function(req, res) {
+    //parse the url of the request
+    var parsedUrl = url.parse(req.url, true);
 
-//create comment array
-const comments = [
-  {
-    id: 1,
+    //get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    //get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    //get the HTTP method
+    var method = req.method.toLowerCase();
+
+    //send the response
+    res.end('Hello World\n');
+
+    //log the request path
+    console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters', queryStringObject);
+}).listen(3000, function() {
+    console.log('Server listening on port 3000');
+});
