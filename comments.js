b3b6b010a0fa4555f62aab89f75902610b001c07
aaronappleseed <noseebnoraa@gmail.com>
2025-02-01 16:55:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,16 @@
 //create web server
-var http = require('http');
-var url = require('url');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-//create server object
-http.createServer(function(req, res) {
-    //parse the url of the request
-    var parsedUrl = url.parse(req.url, true);
-
-    //get the path
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    //get the query string as an object
-    var queryStringObject = parsedUrl.query;
-
-    //get the HTTP method
-    var method = req.method.toLowerCase();
-
-    //send the response
-    res.end('Hello World\n');
-
-    //log the request path
-    console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters', queryStringObject);
-}).listen(3000, function() {
-    console.log('Server listening on port 3000');
+// Create a new file to store the comments
+fs.open('comments.txt', 'a', function (err, fd) {
+  if (err) throw err;
+  fs.close(fd, function (err) {
+    if (err) throw err;
+  });
 });
+
+app.use(express.static('public'));
