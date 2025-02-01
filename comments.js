05b16d7ea3d104b0d1279b427f9585ffd68e3b06
aaronappleseed <noseebnoraa@gmail.com>
2025-02-01 17:13:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,5 @@
-//create web server
-//create comments.js file
-//create a server that listens on port 3000
-//create a route that will respond with the text "Hello World" when a GET request is made to /comments
-//export the server
-
-const http = require('http');
-const server = http.createServer((req, res) => {
-    if (req.url === '/comments') {
-        res.write('Hello World');
-        res.end();
-    }
-});
-server.listen(3000);
-module.exports = server;
-//run the test
-//npm test comments.js
+// Create web server
+// Create a route for comments
+// Create a route for comments/:id
+// Create a route for comments/:id/edit
+// Create a route for comments/:id/delete
