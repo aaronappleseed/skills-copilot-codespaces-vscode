7daa39e5f29320f2ce2017dd79edaa0cb9d10ac3
aaronappleseed <noseebnoraa@gmail.com>
2025-02-01 17:10:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,17 @@
 //create web server
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+//create comments.js file
+//create a server that listens on port 3000
+//create a route that will respond with the text "Hello World" when a GET request is made to /comments
+//export the server
 
-// Create a new file to store the comments
-fs.open('comments.txt', 'a', function (err, fd) {
-  if (err) throw err;
-  fs.close(fd, function (err) {
-    if (err) throw err;
-  });
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/comments') {
+        res.write('Hello World');
+        res.end();
+    }
 });
-
-app.use(express.static('public'));
+server.listen(3000);
+module.exports = server;
+//run the test
+//npm test comments.js
